744123fe084e936b457f82afe9f36129276158a1
rafamaster3 <rafaelfernandez90@gmail.com>
2025-07-13 15:16:31 -0400

    Update Variable.cs

--- Assets/SO Variables/Variables/Scripting/Variable.cs
+++ Assets/SO Variables/Variables/Scripting/Variable.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+/// <summary>
+/// Creates a base class for all variable types in the project
+/// so all types can be referenced by just one VariableReference script 
+/// instead of being a different script to reference each type./// 
+/// </summary>
+
 public abstract class Variable<T> : ScriptableObject
 {
     public T Value;
